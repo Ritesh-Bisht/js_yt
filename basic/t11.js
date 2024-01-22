6aa718fd6e71bbc4129f580bdec0769430dfcc12
Ritesh-Bisht <141986335+Ritesh-Bisht@users.noreply.github.com>
2024-01-22 14:22:12 +0000

    changes saved - 2

---
+++ basic/t11.js
@@ -0,0 +1,29 @@
+// treat all js code as newer version
+console.log("hello world")
+const accountId = 51
+let accountEmail='gmail.com'
+var accountPassword=1001;
+acountCity="dun"
+let accountState
+// accountId=2   assignment to const variable isn't allowed 
+accountEmail='world@'
+acountCity="dehradun"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,acountCity,accountState])
+let sc=1
+console.log(typeof sc)
+console.log(typeof (sc))
+let val=String(sc)
+console.log(typeof (val))
+console.log(val)
+
+let s1="hello "
+let s2= "world"
+console.log(s1+s2)
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"2")
+console.log("1"+2+2)
+let n1,n2,n3
+n1=n2=n3=6
+
