12af8cec52675848f6d79325ce122898ce18647b
RITESH BISHT <141986335+Ritesh-Bisht@users.noreply.github.com>
2024-02-09 15:18:00 +0000

    functions done

---
+++ basic/fun3.js
@@ -0,0 +1,50 @@
+function addt(n1,n2)
+{
+    console.log(n1+n2)
+}
+function add2(n1,n2)
+{
+    return n1+n2
+}
+addt(6,9)
+addt(6,"9")
+addt(6,"a")
+res=add2(6,9)
+console.log("result is : ",res)
+function log(u="sam")
+{
+    if(!u){
+        console.log(" please enter a username ")
+    return 
+}
+    return`${u} just logged in`
+}
+console.log(log("ritesh"))
+console.log(log())
+function cal(...n1)
+{
+    return n1
+}
+console.log(cal(20,70))
+console.log(cal(200,300,400,500))
+const u1={
+    u:"rb",
+    price:200
+}
+function f1(any)
+{
+    console.log(`username is ${any.u} and price is ${any.price}`)
+}
+console.log()
+f1(u1)
+f1({
+    u:"h",
+    price:300
+})
+const arr = [2,3,4,5]
+function fc2(getarr)
+{
+    return getarr[2]
+}
+console.log(fc2(arr))
+console.log(fc2([20,30,40,50]))
