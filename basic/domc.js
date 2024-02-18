292eaf62634b6ff2c9b46e97edab746f207f8416
RITESH BISHT <141986335+Ritesh-Bisht@users.noreply.github.com>
2024-02-18 07:03:59 +0000

    creating new object using DOM

--- basic/domc.js
+++ basic/domc.js
@@ -1,1 +1,38 @@
 console.log(`dom start`)
+myarr.forEach( (l)=>{
+    l.style.color='orange'
+})
+const myarr= Array.from(temp)
+
+document.getElementById('title')
+document.getElementById('title').id
+document.getElementById('title').className
+title.style.backgroundColor='green'
+title.style.borderRadius="25px"
+document.getElementById('title')
+document.getElementById('title').getAttribute('id')
+document.getElementById('title').setAttribute('class','test')
+
+const title=document.getElementById('title')
+title.style.backgroundColor='green'
+title.innerHTML
+title.innerText
+title.textContent
+
+document.getElementsByClassName('heading')
+document.querySelector('h2')
+document.querySelector('#title')
+document.querySelector('.heading')
+document.querySelector('input[type="password"]')
+const myul = document.querySelector('ul')
+const tgreen = myul.querySelector('li')
+tgreen.style.backgroundColor="green"
+tgreen.style.padding="10px"
+tgreen.innerText="one1"
+document.querySelectorAll('li')
+const arr = document.querySelectorAll('li')
+arr[0].style.color='green'
+const myh1 = document.querySelectorAll('h1')
+myh1[0].style.color='green'
+const temp= document.getElementsByClassName('list_item')
+console.dir()
