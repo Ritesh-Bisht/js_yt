c04c783129dec2d9ef5edb122ec4a5e81e1adbc9
RITESH BISHT <141986335+Ritesh-Bisht@users.noreply.github.com>
2024-03-24 16:00:23 +0000

    gettrs and setters

---
+++ basic/get_set.js
@@ -0,0 +1,16 @@
+class user {
+    constructor(email, password ){
+        this.email=email
+        this.password=password
+    }
+}
+const desc = Object.getOwnPropertyDescriptor(Math, PI)
+console.log(desc) 
+
+const chai = {
+    name:'ginger',
+    price:250,
+    is:true
+}
+
+console.log(chai);
