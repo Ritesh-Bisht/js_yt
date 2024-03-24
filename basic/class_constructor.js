cb2f3b33f8420916b79deacb942e1a93f181f569
RITESH BISHT <141986335+Ritesh-Bisht@users.noreply.github.com>
2024-03-24 07:16:26 +0000

    class and constructor done

--- basic/class_constructor.js
+++ basic/class_constructor.js
@@ -27,7 +27,7 @@ console.log(chai.changecase())
 
  }
 
-class human{
+class human{ // parent : human class 
     constructor(name){
         this.name=name;
     }
@@ -35,17 +35,46 @@ class human{
         console.log(`USERNAME is ${this.name}`);
     }
 }
-class teacher extends human{
+class teacher extends human{ // child : teacher class
     constructor(name, email, password){
         super(name) // this will work as .call(this, name)
         this.email=email
         this.password=password
     }
     addCourse(){
-        console.log(`A new added by ${this.name}` )
+        console.log(`A new course added by ${this.name}` )
     }
 }
- const c = new teacher("chai", "@gmail",123)
+const c = new teacher("chai", "@gmail",123)
+c.addCourse()
+c.logme()
 
- c.addCourse()
+const t = new human("masala")
+t.logme()
+console.log(c instanceof(human));
+
+class u1{
+    constructor(name){
+        this.name=name
+    }
+    logme(){
+        console.log(`USERNAME : ${this.name}`);
+    }
+    static createId(){
+        return `123`
+    }
+}
+
+const h = new u1("hit")
+// console.log(h.createId()); // static can not access
+
+class u2 extends u1{
+    constructor(name, email){
+        super(name)
+        this.email = email
+    }
+}
+
+const i = new u2("phone", "@gmail")
+console.log(i.createId())
 
